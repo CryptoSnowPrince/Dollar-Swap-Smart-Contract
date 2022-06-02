612f6b753fe8aba1cbff4f0b916b7255e7b13a1d
CryptoSnowPrince <topdirector2017@gmail.com>
2022-06-02 13:33:46 +0200

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -54,21 +54,49 @@ const config: HardhatUserConfig = {
       },
     ]
   },
+  defaultNetwork: "hardhat",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 40,
+        passphrase: "",
+      },
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    bsc: {
+      url: process.env.BSC_URL || "",
+      accounts:
+        process.env.BSC_GOOSEBUMPS_PRIVATE_KEY !== undefined ? [process.env.BSC_GOOSEBUMPS_PRIVATE_KEY] : [],
     },
     bsctestnet: {
       url: process.env.BSCTESTNET_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    tethermoon: {
-      url: process.env.TETHERMOON_URL || "",
+    roburna: {
+      url: process.env.ROBURNA_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygon: {
+      url: process.env.POLYGON_URL || "",
+      accounts:
+        process.env.POLYGON_GOOSEBUMPS_PRIVATE_KEY !== undefined ? [process.env.POLYGON_GOOSEBUMPS_PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
@@ -76,8 +104,48 @@ const config: HardhatUserConfig = {
     currency: "USD",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    apiKey: {
+        mainnet: process.env.ETHERSCAN_API_KEY,
+        ropsten: process.env.ETHERSCAN_API_KEY,
+        rinkeby: process.env.ETHERSCAN_API_KEY,
+        goerli: process.env.ETHERSCAN_API_KEY,
+        kovan: process.env.ETHERSCAN_API_KEY,
+        // binance smart chain
+        bsc: process.env.BSCSCAN_API_KEY,
+        bscTestnet: process.env.BSCSCAN_API_KEY,
+        // huobi eco chain
+        heco: "YOUR_HECOINFO_API_KEY",
+        hecoTestnet: "YOUR_HECOINFO_API_KEY",
+        // fantom mainnet
+        opera: "YOUR_FTMSCAN_API_KEY",
+        ftmTestnet: "YOUR_FTMSCAN_API_KEY",
+        // optimism
+        optimisticEthereum: "YOUR_OPTIMISTIC_ETHERSCAN_API_KEY",
+        optimisticKovan: "YOUR_OPTIMISTIC_ETHERSCAN_API_KEY",
+        // polygon
+        polygon: process.env.POLYGON_API_KEY,
+        polygonMumbai: process.env.POLYGON_API_KEY,
+        // arbitrum
+        arbitrumOne: "YOUR_ARBISCAN_API_KEY",
+        arbitrumTestnet: "YOUR_ARBISCAN_API_KEY",
+        // avalanche
+        avalanche: "YOUR_SNOWTRACE_API_KEY",
+        avalancheFujiTestnet: "YOUR_SNOWTRACE_API_KEY",
+        // moonbeam
+        moonbeam: "YOUR_MOONBEAM_MOONSCAN_API_KEY",
+        moonriver: "YOUR_MOONRIVER_MOONSCAN_API_KEY",
+        moonbaseAlpha: "YOUR_MOONBEAM_MOONSCAN_API_KEY",
+        // harmony
+        harmony: "YOUR_HARMONY_API_KEY",
+        harmonyTest: "YOUR_HARMONY_API_KEY",
+        // xdai and sokol don't need an API key, but you still need
+        // to specify one; any string placeholder will work
+        xdai: "api-key",
+        sokol: "api-key",
+        aurora: "api-key",
+        auroraTestnet: "api-key",
+    }
+  }
 };
 
 export default config;
